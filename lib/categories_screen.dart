2ea0e22f1215b83a119f359b457b4a21068cf122
wilhge <flankorn1@gmail.com>
2019-12-30 13:52:27 -0400

    created custom theme and to apply in list of categories

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Food App"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
