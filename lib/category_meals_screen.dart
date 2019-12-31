9cb07b855cac32db1e91113a7a2a239aa4f3f09c
wilhge <flankorn1@gmail.com>
2019-12-30 23:57:57 -0400

    listView Builder to show meals

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
+    final routeArg =
+        ModalRoute.of(ctx).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArg['title'];
+    final categoryId = routeArg['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipe'),
+        title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipes for The Cateogry!'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
